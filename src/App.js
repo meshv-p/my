34672b7f4c8114012762e2c229649224d7951a15
Meshv Patel <77038149+meshv-p@users.noreply.github.com>
2021-07-23 18:48:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import reactDom from 'react-dom';
+import './App.css';
+import Header from './mycompo/Header';
+import {Footer} from './mycompo/Footer';
+
+
+
+function App(){
+
+    return(
+
+    <>
+     <Header title="Meshv"/>
+
+     <Footer />
+    </>
+  );
+}
+
+
+// export default Meshv;
+export default  App;
+// export {Meshv , Try }  ; 
+// export {Try}; 
